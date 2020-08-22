cffb06b20d39895f1a1cfc05683c2353cede27ca
Kole Nunley <Kfnunley@gmail.com>
2020-08-22 13:01:08 -0400

    move old files

---
+++ client/src/old/World.gd
@@ -0,0 +1,237 @@
+extends Node
+
+export var CharacterScene: PackedScene
+export var ProjectileScene : PackedScene
+
+var characters := {}
+var _projectiles = {}
+
+onready var player : Player = $Player
+
+func _ready() -> void:
+	#warning-ignore: return_value_discarded
+	MatchController.connect(
+		"initial_state_received", self, "_on_initial_state_received"
+	)
+		#warning-ignore: return_value_discarded
+	MatchController.connect("users_changed", self, "_on_users_changed")
+	#warning-ignore: return_value_discarded
+	MatchController.connect("state_updated", self, "_on_state_updated")
+
+	
+func _on_initial_state_received(
+	positions: Dictionary, 
+	rotations: Dictionary, 
+	inputs: Dictionary, 
+	names: Dictionary,
+	_targets: Dictionary,
+	healths: Dictionary,
+	powers: Dictionary,
+	casts: Dictionary,
+	projectiles: Dictionary,
+	effects: Dictionary,
+	speeds: Dictionary
+) -> void:
+	var user_id: String = ServerConnection.get_user_id()
+	var username: String = names.get(user_id)
+	var p = positions[user_id]
+	var r = rotations[user_id]
+	var player_position := Vector3(p.x, p.y, p.z)
+	var player_rotation := Vector3(r.x, r.y, r.z)
+	player.setup(username, player_position, player_rotation, healths[user_id], powers[user_id], effects[user_id], speeds[user_id])
+	player.connect("input_event", self, "_on_character_input_event", [ ServerConnection.get_user_id() ])
+	
+	var users = MatchController.users
+	for id in users.keys():
+		if id in positions:
+			var user_p = positions[id]
+			var character_position := Vector3(user_p.x, user_p.y, user_p.z)
+			var user_dir = inputs[id].dir
+			var character_direction := Vector3(user_dir.x, user_dir.y, user_dir.z)
+			var user_rot = rotations[id]
+			var character_rotation := Vector3(user_rot.x, user_rot.y, user_rot.z)
+			if not id in characters:
+				create_character(
+					id, 
+					names[id], 
+					character_position, 
+					character_rotation,
+					character_direction,
+					healths[id],
+					powers[id],
+					casts,
+					effects[id],
+					speeds[id]
+				)
+			else:
+				var character = characters[id]
+				character.next_position = character_position
+				character.next_rotation = character_rotation
+				character.next_input = character_direction
+				character.next_jump = inputs[id].jmp == 1
+				character.health = healths[id]
+				character.power = powers[id]
+				character.effects = effects[id]
+				character.speed = speeds[id]
+				character.update_state()
+				character.spawn()
+				if id in casts:
+					character.set_cast(casts[id])
+					
+	_update_projectiles(projectiles)
+
+func create_character(
+	id: String,
+	username: String,
+	position: Vector3,
+	rotation: Vector3,
+	direction: Vector3,
+	health: int,
+	power: int,
+	casts: Dictionary,
+	effects: Array,
+	speed: float
+) -> void:
+	var character := CharacterScene.instance()
+	#warning-ignore: return_value_discarded
+	get_parent().add_child(character)
+	character.direction = direction
+	character.username = username
+	character.health = health
+	character.power = power
+	character.effects = effects
+	character.speed = speed
+	if id in casts:
+		character.set_cast(casts[id])
+	character.set_global_position(position)
+	character.set_rotation(rotation)
+	character.spawn()
+	character.connect("input_event", self, "_on_character_input_event", [ id ])
+	characters[id] = character
+
+func _on_users_changed() -> void:
+	var users = MatchController.users
+
+	for key in users:
+		if not key in characters:
+			create_character(key, users[key].username, Vector3.ZERO, Vector3.FORWARD, Vector3.ZERO, 100, 100, {}, [], ServerConnection.game_config.default_speed)
+
+	var to_delete := []
+	for key in characters.keys():
+		if not key in users:
+			to_delete.append(key)
+
+	for key in to_delete:
+		
+		if player.target == characters[key]:
+			player.target = null
+	
+		characters[key].despawn()
+		#warning-ignore: return_value_discarded
+		characters.erase(key)
+		
+func _on_state_updated(
+	positions: Dictionary, 
+	rotations: Dictionary, 
+	inputs: Dictionary, 
+	targets: Dictionary, 
+	healths: Dictionary, 
+	powers: Dictionary,
+	casts: Dictionary,
+	projectiles: Dictionary,
+	effects: Dictionary,
+	speeds: Dictionary
+) -> void:
+	var player_id = ServerConnection.get_user_id()
+	player.health = healths[player_id]
+	player.power = powers[player_id]
+	player.effects = effects[player_id]
+	player.speed = speeds[player_id]
+	
+	if positions[player_id].has("override"):
+		var p : Dictionary = positions[player_id]
+		player.next_position = Vector3(p.x, p.y, p.z)
+		player.next_rotation = player.global_transform.basis.z
+		player.update_state()
+	
+	if player_id in casts:
+		player.set_cast(casts[player_id])
+	else:
+		player.cancel_cast()
+	
+	for key in characters:
+		var character = characters[key]
+		if key in positions:
+			var p : Dictionary = positions[key]
+			character.next_position = Vector3(p.x, p.y, p.z)
+		if key in rotations:
+			var r : Dictionary = rotations[key]
+			character.next_rotation = Vector3(r.x, r.y, r.z)
+		if key in inputs:
+			var dir = inputs[key].dir
+			character.next_input = Vector3(dir.x, dir.y, dir.z)
+			character.next_jump = inputs[key].jmp == 1
+		if key in targets:
+			if targets[key] in characters:
+				character.target = characters[targets[key]]
+			elif targets[key] == ServerConnection.get_user_id():
+				character.target = player
+		else:
+			character.target = null
+		if key in healths:
+			character.health = healths[key]
+		if key in powers:
+			character.power = powers[key]
+		if key in effects:
+			character.effects = effects[key]
+		if key in casts:
+			character.set_cast(casts[key])
+		if key in speeds:
+			character.speed = speeds[key]
+		else:
+			character.cancel_cast()
+				
+		character.update_state()
+		
+	_update_projectiles(projectiles)
+			
+
+func _on_character_input_event(
+	_camera : Node, 
+	event : InputEvent, 
+	_click_position : Vector3, 
+	_click_normal : Vector3, 
+	_shape_idx : int, 
+	target_id : String
+) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			var prev_target = player.target
+			if target_id == ServerConnection.get_user_id():
+				player.target = player
+			elif target_id in characters:
+				player.target = characters[target_id]
+			if prev_target != player.target:
+				MatchController.send_target(target_id)
+				
+func _update_projectiles(latest_projectiles : Dictionary) -> void:
+	for id in latest_projectiles:
+		var proj = latest_projectiles[id]
+		var proj_pos = Vector3(proj.position.x, proj.position.y, proj.position.z)
+		if id in _projectiles:
+			_projectiles[id].update_position(proj_pos)
+		else:
+			var instance = ProjectileScene.instance()
+			add_child(instance)
+			instance.update_position(proj_pos)
+			_projectiles[id] = instance
+		
+		if proj.to_id in characters:
+			_projectiles[id].look_at(characters[proj.to_id].get_node("LineOfSitePoint").global_transform.origin, Vector3.UP)
+		elif proj.to_id == ServerConnection.get_user_id():
+			_projectiles[id].look_at(player.get_node("LineOfSitePoint").global_transform.origin, Vector3.UP)
+	
+	for id in _projectiles:
+		if not id in latest_projectiles:
+			_projectiles[id].queue_free()
+			_projectiles.erase(id)
