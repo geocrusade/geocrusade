02f1eb665dd73a483cc6c8f03c0da6ba140f5e64
Kole Nunley <Kfnunley@gmail.com>
2020-08-25 21:00:25 -0400

    use color picker

--- client/src/SkinColorChangerUI.gd
+++ client/src/SkinColorChangerUI.gd
@@ -1,29 +1,40 @@
 extends Control
 
+signal color_saved(color)
+
 export var skin_material : SpatialMaterial
 
-onready var _red_slider = $RedSlider
-onready var _green_slider = $GreenSlider
-onready var _blue_slider = $BlueSlider
-onready var _red_label = $RedSlider/RedLabel
-onready var _green_label = $GreenSlider/GreenLabel
-onready var _blue_label = $BlueSlider/BlueLabel
-onready var _color_rect = $ColorRect
+onready var _picker : ColorPicker = $ColorPicker
+onready var _edit_button : Button = $EditButton
+onready var _save_button : Button = $SaveButton
+onready var _cancel_button : Button = $CancelButton
 
+onready var _saved_color : Color = skin_material.albedo_color
 
 func set_skin_color(color : Color):
 	skin_material.albedo_color = color
 
 func _ready():
-	_red_slider.connect("value_changed", self, "_value_changed", [_red_label])
-	_green_slider.connect("value_changed", self, "_value_changed", [_green_label])
-	_blue_slider.connect("value_changed", self, "_value_changed", [_blue_label])
-	
-func _value_changed(value : float, label : Label):
-	label.text = String(value)
-	var color = _get_color()
-	_color_rect.color = color
-	set_skin_color(color)
+	_picker.color = _saved_color
+	_picker.connect("color_changed", self, "set_skin_color")
+	_edit_button.connect("button_up", self, "_edit")
+	_save_button.connect("button_up", self, "_save")
+	_cancel_button.connect("button_up", self, "_reset")
 
-func _get_color() -> Color:
-	return Color8(int(_red_label.text), int(_green_label.text), int(_blue_label.text))
+func _edit():
+	_picker.show()
+	_save_button.show()
+	_cancel_button.show()
+	_edit_button.hide()
+
+func _save():
+	_saved_color = skin_material.albedo_color
+	emit_signal("color_saved", _saved_color)
+	_reset()
+	
+func _reset():
+	_picker.hide()
+	_save_button.hide()
+	_cancel_button.hide()
+	_edit_button.show()
+	set_skin_color(_saved_color)
