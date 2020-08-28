c1ff08fd65e7d2ac189f78797affa9fee878897b
Kole Nunley <Kfnunley@gmail.com>
2020-08-28 14:20:34 -0400

    rm lua module code

--- server/modules/hooks.lua
+++
@@ -1,7 +0,0 @@
-local nk = require("nakama")
-
-local function on_matchmaker_matched(context, matched_users)
-  return nk.match_create("match_handler", { matched_users = matched_users, is_arena = true })
-end
-
-nk.register_matchmaker_matched(on_matchmaker_matched)
