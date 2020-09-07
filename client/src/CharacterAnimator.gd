8c2e1ed83a2be0c985033285670817823d0449e5
Kole Nunley <Kfnunley@gmail.com>
2020-09-07 11:54:21 -0400

    add back smoothing

--- client/src/CharacterAnimator.gd
+++ client/src/CharacterAnimator.gd
@@ -5,30 +5,38 @@ onready var _mesh_root : Spatial = get_parent().get_node("Armature")
 
 var _iwr_blend = Vector2.ZERO
 var _target_iwr_blend = Vector2.ZERO
+var _target_rot_y = 180
 var blend_rate : float = 0.2
+
+func _process(delta):
+	_iwr_blend = _iwr_blend.linear_interpolate(_target_iwr_blend, blend_rate)
+	_animation_tree.set("parameters/iwr/blend_position", _iwr_blend)
+
+func _physics_process(delta):
+	_mesh_root.rotation_degrees.y = lerp(_mesh_root.rotation_degrees.y, _target_rot_y, blend_rate)
 
 func animate_movement(direction : Vector3):
 	_target_iwr_blend = Vector2(round(direction.x), round(-direction.z))
-	_animation_tree.set("parameters/iwr/blend_position", _target_iwr_blend)
 	var default_deg = 180
-	
 	match _target_iwr_blend:
 		Vector2.DOWN:
-			_mesh_root.rotation_degrees.y = default_deg
+			_target_rot_y = default_deg
 		Vector2.LEFT:
-			_mesh_root.rotation_degrees.y = default_deg + 90
+			_target_rot_y = default_deg + 90
 		(Vector2.DOWN + Vector2.LEFT):
-			_mesh_root.rotation_degrees.y = default_deg + 45
+			_target_rot_y = default_deg + 45
 		(Vector2.UP + Vector2.LEFT):
-			_mesh_root.rotation_degrees.y = default_deg - 45
+			_target_rot_y = default_deg - 45
 		Vector2.UP:
-			_mesh_root.rotation_degrees.y = default_deg 
+			_target_rot_y = default_deg 
 		(Vector2.UP + Vector2.RIGHT):
-			_mesh_root.rotation_degrees.y = default_deg + 45
+			_target_rot_y = default_deg + 45
 		Vector2.RIGHT:
-			_mesh_root.rotation_degrees.y = default_deg - 90
+			_target_rot_y = default_deg - 90
 		(Vector2.DOWN + Vector2.RIGHT):
-			_mesh_root.rotation_degrees.y = default_deg - 45
+			_target_rot_y = default_deg - 45
+		Vector2.ZERO:
+			_target_rot_y = default_deg
 
 func jump():
 	_animation_tree.set("parameters/jump/active", true)
